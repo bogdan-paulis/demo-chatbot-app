65ab128986a9ad2ef09ad8c47cf249bd9e76a1de
bogdan-paulis <bogdan.paulis@tum.de>
2017-12-18 17:41:01 +0100

    Add files via upload
    Init

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=demo-chatbot-app-Web-Deploy.pubxml -p:Password=Zng4BEmWSnuSMMFyDBuAZYv7XszRLoCTnatcMBFpikelrNjJM22bPfK5Q3SY
+
